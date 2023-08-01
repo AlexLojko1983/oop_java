cf8b5d4505eed74697c4a96102dedda7d1de5566
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-08-01 16:10:34 +0300

    add dz3

---
+++ src/main/java/org/geekbrains/lesson4/homework3/models/Teacher.java
@@ -0,0 +1,9 @@
+package org.geekbrains.lesson4.homework3.models;
+
+import java.time.LocalDate;
+
+public class Teacher extends User{
+    public Teacher(String firstName, String secondName, String patronymic, LocalDate dateOfBirth) {
+        super(firstName, secondName, patronymic, dateOfBirth);
+    }
+}
